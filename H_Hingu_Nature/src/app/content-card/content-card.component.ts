81973449a0a7e7396ad91d9f2435cbbb64dfb96d
harshilhingu <91302645+harshilhingu@users.noreply.github.com>
2022-07-05 19:49:35 -0400

    done

--- H_Hingu_Nature/src/app/content-card/content-card.component.ts
+++ H_Hingu_Nature/src/app/content-card/content-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../models/content';
 
 @Component({
   selector: 'app-content-card',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-
+@Input() natureItem?: Content
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  printConsole(){
+    console.log(this.natureItem?.author);
+    console.log(this.natureItem?.id);
   }
 
 }
